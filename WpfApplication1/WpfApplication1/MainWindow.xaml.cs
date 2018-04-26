a5da7f63aa721790c5458192a7db4ce9d8121e21
MaxCh330 <asdfgtrt@gmail.com>
2018-04-26 16:38:36 +0800

    BMI Finish

---
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void tallclear_Click(object sender, RoutedEventArgs e)
+        {
+            talltext.Text = "";
+        }
+
+        private void weigh_Click(object sender, RoutedEventArgs e)
+        {
+            weightext.Text = "";
+        }
+
+        private void finishbutton_Click(object sender, RoutedEventArgs e)
+        {
+            float tall = float.Parse(talltext.Text);
+            float weigh = float.Parse(weightext.Text);
+            float bmi;
+            
+            bmi = weigh / (tall/100 * tall/100);
+            bmitext.Text = bmi.ToString();
+        }
+
+    }
+}
